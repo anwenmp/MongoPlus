804c51177f7fb57b0386f63c277c5931dd1e74b3
anwen <j15030047216@163.com>
2024-12-04 17:52:33 +0800

    替换器获取参数生命

--- mongo-plus-core/src/main/java/com/anwen/mongo/replacer/Replacer.java
+++ mongo-plus-core/src/main/java/com/anwen/mongo/replacer/Replacer.java
@@ -1,7 +1,9 @@
 package com.anwen.mongo.replacer;
 
 import com.anwen.mongo.support.BoolFunction;
+import com.mongodb.client.MongoCollection;
 
+import javax.swing.text.Document;
 import java.lang.reflect.Method;
 
 /**
@@ -16,7 +18,22 @@ public interface Replacer {
         return Integer.MIN_VALUE;
     }
 
+    /**
+     * 执行方法
+     *
+     * @param proxy 代理对象
+     * @param target 目标对象
+     * @param method 方法
+     * @param args 参数 数组的最后一个值为 MongoCollection<Document>
+     * @return Object
+     * @author JiaChaoYang
+     */
     Object invoke(Object proxy, Object target, Method method, Object[] args) throws Throwable;
+
+    @SuppressWarnings("unchecked")
+    default MongoCollection<Document> getMongoCollection(Object[] args) {
+        return (MongoCollection<Document>) args[args.length-1];
+    }
 
     BoolFunction supplier();
 
